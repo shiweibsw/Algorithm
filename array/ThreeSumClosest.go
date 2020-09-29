03df3989160297082af675596fe2d4aa57bd407f
shiweibsw <shiweibsw@gmail.com>
2020-09-29 16:00:20 +0800

    ThreeSumClosest.go

---
+++ array/ThreeSumClosest.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"sort"
+)
+
+/**
+给定一个数组，要求在这个数组中找出 3 个数之和离 target 最近。
+Given array nums = [-1, 2, 1, -4], and target = 1.
+The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+思路：这一题的解法是用两个指针夹逼的方法。先对数组进行排序，i 从头开始往后面扫。这里同样需要注意数组中存在多个重复数字的问题。具体处理方法很多，可以用 map 计数去重。这里笔者简单的处理，i 在循环的时候和前一个数进行比较，如果相等，i 继续往后移，直到移到下一个和前一个数字不同的位置。j，k 两个指针开始一前一后夹逼。j 为 i 的下一个数字，k 为数组最后一个数字，由于经过排序，所以 k 的数字最大。j 往后移动，k 往前移动，逐渐夹逼出最接近 target 的值。
+时间复杂度：O(n^2)，空间复杂度：O(1)
+*/
+func main() {
+	arr := []int{-1, 2, 1, -4}
+	threeSumClosest(arr, 1)
+}
+
+func threeSumClosest(nums []int, target int) int {
+	n, res, diff := len(nums), 0, math.MaxInt32
+	if n > 2 {
+		sort.Ints(nums)
+		for i := 0; i < n-2; i++ {
+			for j, k := i+1, n-1; j < k; {
+				fmt.Println(j)
+				sum := nums[i] + nums[j] + nums[k]
+				if abs(sum-target) < diff {
+					res, diff = sum, abs(sum-target)
+				}
+				if sum == target {
+					return res
+				} else if sum > target {
+					k--
+				} else {
+					j++
+				}
+			}
+		}
+	}
+	return res
+}
+
+func abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
+}
--- array/ThreeSumEqualZero.go
+++ array/ThreeSumEqualZero.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 /**
 给定一个数组，要求在这个数组中找出 3 个数之和为 0 的所有组合
 Given array nums = [-1, 0, 1, 2, -1, -4],
@@ -8,9 +13,45 @@ A solution set is:
   [-1, 0, 1],
   [-1, -1, 2]
 ]
-思路：
+思路：用 map 提前计算好任意 2 个数字之和，保存起来，可以将时间复杂度降到 O(n^2)。这一题比较麻烦的一点在于，最后输出解的时候，要求输出不重复的解。数组中同一个数字可能出现多次，同一个数字也可能使用多次，但是最后输出解的时候，不能重复。例如 [-1，-1，2] 和 [2, -1, -1]、[-1, 2, -1] 这 3 个解是重复的，即使 -1 可能出现 100 次，每次使用的 -1 的数组下标都是不同的。
 
+这里就需要去重和排序了。map 记录每个数字出现的次数，然后对 map 的 key 数组进行排序，最后在这个排序以后的数组里面扫，找到另外 2 个数字能和自己组成 0 的组合。
+
+时间复杂度：O(n^2) 空间复杂度：O(n)
 */
 func main() {
+	arr := []int{-1, 0, 1, 2, -1, -4}
+	fmt.Println(threeSum(arr))
+}
 
+func threeSum(nums []int) [][]int {
+	res := [][]int{}
+	counter := map[int]int{}
+	for _, value := range nums {
+		counter[value]++ //记录数组元素出现的次数，key为数组元素，值为次数
+	}
+	uniqNums := []int{} //去重后的数组
+	for key := range counter {
+		uniqNums = append(uniqNums, key)
+	}
+	sort.Ints(uniqNums)
+	for i := 0; i < len(uniqNums); i++ {
+		if uniqNums[i]*3 == 0 && counter[uniqNums[i]] >= 3 {
+			res = append(res, []int{uniqNums[i], uniqNums[i], uniqNums[i]})
+		}
+		for j := i + 1; j < len(uniqNums); j++ {
+			if uniqNums[i]*2+uniqNums[j] == 0 && counter[uniqNums[i]] > 1 {
+				res = append(res, []int{uniqNums[i], uniqNums[i], uniqNums[j]})
+			}
+			if uniqNums[j]*2+uniqNums[i] == 0 && counter[uniqNums[j]] > 1 {
+				res = append(res, []int{uniqNums[i], uniqNums[j], uniqNums[j]})
+			}
+			c := 0 - uniqNums[i] - uniqNums[j]
+			if c > uniqNums[j] && counter[c] > 0 {
+				res = append(res, []int{uniqNums[i], uniqNums[j], c})
+			}
+		}
+
+	}
+	return res
 }
