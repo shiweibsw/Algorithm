cd9126fe1e70f4d183b3d817610cca55d70dcef7
shiweibsw <shiweibsw@gmail.com>
2020-09-30 10:23:42 +0800

    Maximum_Subarray.go

---
+++ array/Maximum_Subarray.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+/**
+给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+思路：
+*/
+func main() {
+	aar := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+	fmt.Println(maxSubArray(aar))
+}
+
+func maxSubArray(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	dp, res := make([]int, len(nums)), nums[0]
+	dp[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		if dp[i-1] > 0 {
+			dp[i] = nums[i] + dp[i-1]
+		} else {
+			dp[i] = nums[i]
+		}
+		res = max1(res, dp[i])
+	}
+	return res
+}
+
+func max1(x1 int, x2 int) int {
+	if x1 >= x2 {
+		return x1
+	} else {
+		return x2
+	}
+}
